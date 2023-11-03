de68cd4a0375f12a06eb2af8970626f42266df31
Liam <liam.desrayaud@next-u.fr>
2023-11-03 16:01:24 +0100

    commit liam

--- apiVols/controllers/controllerVol.js
+++ apiVols/controllers/controllerVol.js
@@ -6,4 +6,151 @@ const getAllFlight = async (req, res) => {
   res.json(allFlight);
 };
 
-module.exports = { getAllFlight };
+const getFlightByNumber = async (req, res) => {
+  try {
+    const flightNumber = req.params.flightNumber;
+
+    // Rechercher les vols en fonction du numéro de vol
+    const vols = await Vol.findAll({
+      where: { flightNumber }
+    });
+
+    if (vols.length === 0) {
+      return res.status(404).json({ error: 'Aucun vol trouvé pour ce numéro de vol' });
+    }
+
+    // Répondre avec les vols trouvés
+    res.json(vols);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur lors de la recherche des vols' });
+  }
+};
+
+const createFlight = async (req, res) => {
+  try {
+    // Récupérer les données du corps de la demande
+    const {
+      flightNumber,
+      origine,
+      destination,
+      date,
+      sieges,
+      informationAeroplane
+    } = req.body;
+
+    // Créer un nouveau vol en utilisant le modèle Sequelize
+    const newVol = await Vol.create({
+      flightNumber,
+      origine,
+      destination,
+      date,
+      sieges,
+      informationAeroplane
+    });
+
+    // Répondre avec le nouveau vol créé
+    res.status(201).json(newVol);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur lors de la création du vol' });
+  }
+};
+
+const updateFlight = async (req, res) => {
+  try {
+    const volId = req.params.id;
+
+    // Rechercher le vol existant en fonction de son ID
+    const existingVol = await Vol.findByPk(volId);
+
+    if (!existingVol) {
+      return res.status(404).json({ error: 'Vol non trouvé' });
+    }
+
+    // Mettre à jour les données du vol en utilisant les données du corps de la demande
+    const {
+      flightNumber,
+      origine,
+      destination,
+      date,
+      sieges,
+      informationAeroplane
+    } = req.body;
+
+    existingVol.flightNumber = flightNumber;
+    existingVol.origine = origine;
+    existingVol.destination = destination;
+    existingVol.date = date;
+    existingVol.sieges = sieges;
+    existingVol.informationAeroplane = informationAeroplane;
+
+    // Enregistrer les modifications dans la base de données
+    await existingVol.save();
+
+    // Répondre avec le vol mis à jour
+    res.json(existingVol);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur lors de la mise à jour du vol' });
+  }
+};
+
+const deleteFlight = async (req, res) => {
+  try {
+    const volId = req.params.id;
+
+    // Rechercher le vol existant en fonction de son ID
+    const existingVol = await Vol.findByPk(volId);
+
+    if (!existingVol) {
+      return res.status(404).json({ error: 'Vol non trouvé' });
+    }
+
+    // Supprimer le vol de la base de données
+    await existingVol.destroy();
+
+    // Répondre avec un message de succès
+    res.json({ message: 'Vol supprimé avec succès' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur lors de la suppression du vol' });
+  }
+}
+
+const getStatutSeat = async (req, res) => {
+  try {
+    const flightNumber = req.params.flightNumber;
+    const seatName = req.params.seatName;
+
+    // Rechercher le vol en fonction du numéro de vol
+    const vol = await Vol.findOne({
+      where: { flightNumber }
+    });
+
+    if (!vol) {
+      return res.status(404).json({ error: 'Vol non trouvé' });
+    }
+
+    // Vérifier si le siège existe dans le vol
+    const siegeArray = JSON.parse(vol.sieges).sieges;
+
+    console.log(siegeArray);
+    const getsiegeinarray = siegeArray.find((siege) => siege.name === seatName);
+    if (!siegeArray || !getsiegeinarray ) {
+      return res.status(404).json({ error: 'Siège non trouvé dans ce vol' });
+    }
+
+    // Répondre avec les informations du siège
+    res.json({
+      flightNumber: vol.flightNumber,
+      ...getsiegeinarray
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur lors de la recherche du siège' });
+  }
+}
+
+
+module.exports = { getAllFlight, createFlight, updateFlight, deleteFlight, getFlightByNumber, getStatutSeat };
--- apiVols/routes/vols_routes.js
+++ apiVols/routes/vols_routes.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllFlight } = require('../controllers/controllerVol');
+const { getAllFlight, createFlight, updateFlight, deleteFlight, getFlightByNumber, getStatutSeat } = require('../controllers/controllerVol');
 
 router.get('/', getAllFlight);
+
+
+router.post('/', createFlight);
+
+  
+router.put('/:id', updateFlight); 
+
+
+router.delete('/:id', deleteFlight);
+
+
+router.get('/:flightNumber', getFlightByNumber );
+
+
+router.get('/:flightNumber/seats/:seatName', getStatutSeat);
+  
+
+  
+
+
+
+  
+  
+  
 
 module.exports = router;
